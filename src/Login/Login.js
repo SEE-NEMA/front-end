66e7505b988b9a4d90ab545c449b6d0d4289ca24
sy_01 <haruowner01@gmail.com>
2023-01-27 11:26:39 +0900

    로그인 디자인 수정본
    * 테두리 추가
    * 위치 조정
    * 하이퍼링크 추가

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,43 +3,38 @@ import './Login.css'
 
 function Login () {
     return (
-       
-<div className="page">
-   
+        
+    <div>
+    <h4 className="titleName">SEEnema에 다시 오신걸 환영해요!</h4>
 
-    <div className="titleWrap">
-    SEEnema에 다시 오신걸 환영해요!
-    </div>
+    <div className="page">
 
     <div className="contentWrap">
-        <div className="inputTitle"> 
-        이메일 주소</div>
-            <div className="inputWrap">
-                <input className="input" 
-                placeholder = "test@gmail.com" />
-    </div>
 
-    <div className="errorMessageWrap">
-    </div>
+        <label className="inputTitle"> 
+        이메일</label>
+        <input className="idinput" />
 
-    <div style={{marginTop : "26px" }} className="inputTitle">
-        비밀번호</div>
-        <div className="inputWrap">
-            <input className="input"
-            placeholder = "영문, 숫자, 특수문자 포함 8자 이상"/>
-    </div>
+        <p></p>
 
-    <div className="errorMessageWrap"> 
-    </div>
-    
-  </div>
+        <label style={{marginTop : "26px" }} className="inputTitle">
+        비밀번호</label>
+        <input className="passwordinput"/>
+   
+        </div>
+    </div> 
 
     <div>
         <button className="bottomButton">
             로그인
         </button>
     </div>
-</div> 
+
+    <h3 className="QuestionText">
+        아직 회원이 아니신가요?  <a href="회원가입">회원가입</a>
+    </h3>
+
+</div>
 ) }
 
 export default Login;
