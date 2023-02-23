fe2ef3115b3a2fe3181c01a08d9a7911bef9dafb
sy_01 <haruowner01@gmail.com>
2023-02-24 01:47:33 +0900

    틀
    ** 이전에 만들어놨던 틀
    ** 작업중인 파일은 수정중

---
+++ src/DetailPage/ConcertDT.js
@@ -0,0 +1,17 @@
+import './ConcertDT.css'
+import React from 'react'
+import { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ConcertDT() {
+    return(
+        <div className = "WrapDT">
+
+
+            
+        </div>
+
+    )
+}
+
+export default ConcertDT;
