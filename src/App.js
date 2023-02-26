c7573634f0580be6bad2aa677cf48e553ee36ec7
sy_01 <haruowner01@gmail.com>
2023-02-26 13:42:19 +0900

    콘서트 상세화면
    * Bootstrap Layout 사용
    * 미완성입니다 .....
    * 키워드 어떻게 넣어야할지 모르겠음

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ConcertDT from './DetailPage/ConcertDT';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+   
+      <ConcertDT></ConcertDT>
+    
   );
 }
 
--- src/DetailPage/ConcertDT.js
+++ src/DetailPage/ConcertDT.js
@@ -2,11 +2,21 @@ import './ConcertDT.css'
 import React from 'react'
 import { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function ConcertDT() {
     return(
         <div className = "WrapDT">
-
+            <Container className = "WrapConcert">
+      <Row className = "ConcertRow">
+        <Col className = "ConcertImage" xs={4} ></Col>
+        <Col className = "ConcertInfo"></Col>
+      </Row>
+      </Container>
+      <p className = "Keyword">키워드 총평</p>
 
             
         </div>
