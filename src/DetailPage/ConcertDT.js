1c8b898d9a5860439b11f42c165607abd223ecbf
sy_01 <haruowner01@gmail.com>
2023-02-27 11:24:13 +0900

    글씨체 추가 / 틀만 완성
    * 틀 usestate 써서 바꿀 예정

--- src/DetailPage/ConcertDT.js
+++ src/DetailPage/ConcertDT.js
@@ -13,7 +13,14 @@ function ConcertDT() {
             <Container className = "WrapConcert">
       <Row className = "ConcertRow">
         <Col className = "ConcertImage" xs={4} ></Col>
-        <Col className = "ConcertInfo"></Col>
+        <Col className = "ConcertInfo">
+          <p className = "Category">뮤지컬</p>
+          <p className = "InfoName">뮤지컬 이름</p>
+          <p className = "InfoSub"> 장소 </p>
+          <p className = "InfoSub"> 공연장 총평 </p>
+          <p className = "InfoSub"> 기간 </p>
+          <p className = "InfoSub"> 러닝타임 </p>
+          </Col>
       </Row>
       </Container>
       <p className = "Keyword">키워드 총평</p>
